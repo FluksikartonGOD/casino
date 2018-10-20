d09a97972d4b6ec2bc3f1f68d314bee464848650
FluksikartonGOD <gantonovfe@gmail.com>
2018-10-20 16:29:43 +0300

    change port to 80

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 const app = express();
 
