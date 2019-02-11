09138f1acfa6251d67d7c28e284f8410d37c4a7b
FluksikartonGOD <gantonovfe@gmail.com>
2019-02-12 00:03:16 +0200

    fix: roullete without refresh

--- public/app.js
+++ public/app.js
@@ -22,19 +22,37 @@
         }
     }
 
-    const default_degree = 1800;
-    
+    const add_degree = 1800;
+    const winHistory = [];
+
+    let lastDegree = 0;
+
     var socket = io.connect('/');
     socket.on('connect', function (data) {
         socket.on("game", function (event) {
             console.log(event)
             if (event.result) {
-                const totalDegree = default_degree + event.result * 10;
-                wheel.style.transform = `rotate(${-totalDegree}deg)`
+
+                const win = event.result;
+                const lastWin = winHistory.length ? winHistory[winHistory.length - 1] : null;
+                let totalDegree
+                if (lastWin) {
+                    const diff = win - lastWin;
+                    if (diff >= 0) {
+                        totalDegree = diff * 10;
+                    } else {
+                        totalDegree = (36 + diff) * 10;
+                    }
+                } else {
+                    totalDegree = win * 10;
+                }
+                lastDegree = lastDegree + add_degree + totalDegree;
+                wheel.style.transform = `rotate(${-lastDegree}deg)`
+                winHistory.push(win)
             }
-            if (event.state === 'betting') {
-                wheel.style.transform = `rotate(0deg)`
-            }
+            // if (event.state === 'betting') {
+            //     wheel.style.transform = `rotate(0deg)`
+            // }
         });
     });
 })();
